a3352d80ee5c569a1bb31e85c64639cd98d6049a
Diego Reis <diegoreso@me.com>
2023-04-15 20:40:29 -0300

    create form validation and css button error

--- js/script.js
+++ js/script.js
@@ -3,26 +3,20 @@
  btn.addEventListener('click', event => {
    event.preventDefault()
 
-   const form = document.querySelector('#form')
-   const name_input = document.forms['form']['name']
-   const monthly = document.querySelector('#monthly')
-   const time = document.querySelector('#time')
-   const tax = document.querySelector('#tax')
+  const name_input = document.forms['form']['name']
+  const monthly = document.querySelector('#monthly')
+  const time = document.querySelector('#time')
+  const tax = document.querySelector('#tax')
+  const containter_inputs = document.querySelector('#container-inputs')
 
   const monthlyParse = parseFloat(monthly.value)
   const timeParse = parseFloat(time.value)
   const taxParse = (parseFloat(tax.value) / 100)
   
-  console.log(monthlyParse)
-  console.log(timeParse)
-  console.log(taxParse)
-  
 
 const calc = {
   expr: `${monthlyParse} * (((1 + ${taxParse}) ^ ${timeParse} - 1) / ${taxParse})`
 }
-
-
 
  fetch('http://api.mathjs.org/v4/',{
    method: 'post',
@@ -31,13 +25,40 @@ const calc = {
    },
    body: JSON.stringify(calc)
  })
- .then(resolve =>resolve.json())
+ .then(resolve => resolve.json())
  .then(data =>{
 
     const result = parseFloat(data.result).toFixed(2)
+    const span = document.querySelector('#span')
+
+    if(!name_input.value || !monthly.value || !time.value || !tax.value){
+      
+      span.classList.add('error')
+      span.textContent = 'Preencha os dados corretamente'
+
+      
+    }else{
+
+      span.classList.remove('error')
+      span.textContent = ''
+
+      
+
+      containter_inputs.innerHTML = 
+      `
+      <h1> Olá ${name_input.value}, juntando R$: ${monthlyParse} todo mês você terá R$: ${result} em ${timeParse} meses.
+
+      <button id="btn-return"> Similar novamente </button>
+      `
+    }
+   
     
-  
+
+
+
  })
+
+ 
  })
 
 
